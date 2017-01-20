2a0c99430d11931f9819da214b011f8f902c3d74
WindChimeRan <1012869439@qq.com>
2017-01-20 20:34:28 +0800

    test

--- img2tfrecords.py
+++ img2tfrecords.py
@@ -2,7 +2,6 @@ import os
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-from scipy import misc
 import cv2
 
 
@@ -12,23 +11,23 @@ class img2tfrecords:
     crop_y = './crop_y/'
     filename = './TFRecord/tfDataSet.tfrecords'
 
-    def extract_x(self,filename):
+    def __extract_x(self,filename):
 
         image = cv2.imread(filename)
         b, g, r = cv2.split(image)
         rgb_image = cv2.merge([r, g, b])
         return rgb_image
 
-    def extract_y(self,filename):
+    def __extract_y(self,filename):
 
         image = Image.open(filename)
         return np.asarray(image,np.uint8)
 
 
-    def _int64_feature(self,value):
+    def __int64_feature(self,value):
         return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-    def _bytes_feature(self,value):
+    def __bytes_feature(self,value):
         return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
     def img2bytes(self):
@@ -43,15 +42,15 @@ class img2tfrecords:
             xname = self.crop_x+imgId
             yname = self.crop_y+imgId
 
-            imgx = self.extract_x(xname)
-            imgy = self.extract_y(yname)
+            imgx = self.__extract_x(xname)
+            imgy = self.__extract_y(yname)
 
             irx = imgx.tostring()
             iry = imgy.tostring()
 
             example = tf.train.Example(features=tf.train.Features(feature={
-                'x_raw': self._bytes_feature(irx),
-                'y_raw': self._bytes_feature(iry)
+                'x_raw': self.__bytes_feature(irx),
+                'y_raw': self.__bytes_feature(iry)
             }))
             writer.write(example.SerializeToString())
 
@@ -79,10 +78,6 @@ if __name__ == '__main__':
 
     main()
 
-    #a.disp_one()
-
-    # x,y = a.read_batch(1000)
-    # print(x.shape,y.shape)
 
 
 
