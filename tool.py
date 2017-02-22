c1583d15eea88aa5bc7aa7e49b81bc18350f428b
WindChimeRan <1012869439@qq.com>
2017-02-22 18:19:57 +0800

    compatible with python2

--- tool.py
+++ tool.py
@@ -4,7 +4,9 @@ def view_bar(num, total):
 
   length = 30
   rate_num = round(num / total * length)
-  r = '\r[%s%s]%d%%' % ("#"*rate_num, " "*(length-rate_num), (num+1) / total * 100, )
+#  r = '\r[%s%s]%d%%' % ("#"*rate_num, " "*(length-rate_num), (num+1) / total * 100, )
+ 
+  r = '\r[%s%s]%d%%' % ("#"*(int)(rate_num), " "*(int)(length-rate_num), (num+1) / total * 100, )
   sys.stdout.write(r)
   sys.stdout.flush()
 
